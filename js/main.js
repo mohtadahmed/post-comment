a4b957a9b0999532960cd2918f5175155a925637
mohtadahmed <mohtadsaeem13@gmail.com>
2022-09-26 14:20:25 +0600

    js event handler added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log('This is the file');
+// Add Event listener to the Submit Button
+document.getElementById('submit-button').addEventListener('click', function(){
+    // Get the input Value from the Text Area
+    const commentBox = document.getElementById('post-comment');
+    const commentValue = commentBox.value
+    // console.log(comemntValue);
+    const commentContainer = document.getElementById('comment-section');
+    // console.log(commentContainer);
+    
+    // Create Element for Appending to the main HTML
+    const p = document.createElement('p');
+    p.innerText = commentValue;
+    // Append to the Main HTMl
+    commentContainer.appendChild(p);
+
+    // Clear the Box after submitting the comment
+    commentBox.value = '';
+})
